fab01af08c42b67276696338fb261744f562f17c
Lina Väppling <lina.wappling@gmail.com>
2021-01-05 17:06:17 +0100

    Log out functionallity

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Col, Row, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const Logout = () => {
+	const { logout } = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		(async () => {
+			await logout();
+			navigate('/login');
+		})();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
-		<div>
-			<p>Logout component</p>
-		</div>
+		<>
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<h3>Logging out</h3>
+					<Spinner animation='border' role='status'>
+						<span className='sr-only'>Loading...</span>
+					</Spinner>
+				</Col>
+			</Row>
+		</>
 	);
 };
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const Navigation = () => {
+	const { currentUser } = useAuth();
+
 	return (
 		<>
 			<Navbar bg='dark' variant='dark'>
@@ -14,9 +17,24 @@ const Navigation = () => {
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ml-auto'>
-							<NavLink to='/login' className='nav-link'>
-								Login
-							</NavLink>
+							{currentUser ? (
+								<NavDropdown
+									title={currentUser.email}
+									id='basic-nav-dropdown'
+								>
+									<NavDropdown.Divider />
+									<NavLink
+										to='/logout'
+										className='dropdown-item'
+									>
+										Log Out
+									</NavLink>
+								</NavDropdown>
+							) : (
+								<NavLink to='/login' className='nav-link'>
+									Log In
+								</NavLink>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
